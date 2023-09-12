6d7ae87b642639a653712eae5b4129020eb1f04f
dninemfive <11084010+dninemfive@users.noreply.github.com>
2023-09-11 19:20:40 -0700

    import bookmark plugin from old project

---
+++ bookmark-plugin/ucm-bookmarks-firefox/script.js
@@ -0,0 +1,34 @@
+console.log("a");
+// https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/bookmarks/search
+let id;
+function doSearch() {
+    function downloadSuccess(msg) {
+        id = msg;
+        console.log(`downloaded file with download id ${msg}.`);
+    }
+    function printError(error) {
+        console.log(`error: ${error}`);
+    }
+    function saveResults(items) {
+        let results = [];
+        for(let item of items) {
+            if(item === null) continue;
+            let desc = item.url;
+            results.push(desc);
+            console.log(desc);
+        }
+        console.log(`found ${results.length} items matching the search`);
+        let url = URL.createObjectURL(new Blob([JSON.stringify(results)], { type: "application/json" }));    
+        let download = browser.downloads.download({url: url, filename: "bookmarks.json"}).then(downloadSuccess, printError);
+    }    
+    let search = browser.bookmarks.search("booru")
+                        .then(saveResults, printError);    
+}
+function revokeUrlAfterDownload(obj) {    
+    if(obj.id === id && obj.state.current === "complete") {
+        URL.revokeObjectURL(obj.url);
+    }    
+}
+browser.browserAction.onClicked.addListener(doSearch);
+browser.downloads.onChanged.addListener(revokeUrlAfterDownload);
+console.log("loaded ucm bookmark extractor");
