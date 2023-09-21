e17eabc1eb3b3d83c3a1030f3853ca1eaa1a5909
dninemfive <11084010+dninemfive@users.noreply.github.com>
2023-09-20 18:31:28 -0700

    poggers

--- maui-app/Pages/CollectionPage.xaml.cs
+++ maui-app/Pages/CollectionPage.xaml.cs
@@ -5,14 +5,28 @@ public partial class CollectionPage : ContentPage
 	public CollectionPage()
 	{
         InitializeComponent();
-		int i = 0;
-		int horizontalItems = 7;
-		foreach(Item item in ItemManager.Items.OrderBy(x => x.Id))
-		{
-			if (++i > 25)
-				break;
-            ItemsHolder.Add(new ThumbnailView(item, 1920 / horizontalItems - 10, item.Id));
-		}
 	}
+    Competition? competition = null;
+    private List<Item>? _items = null;
+    private int _index = 0;
+	public async void LoadItems(object? sender, EventArgs e)
+	{
+        Competition? old = competition;
+        competition = CompetitionSelector.Competition;
+        if(competition?.Name != old?.Name)
+        {
+            _index = 0;
+            _items = await Task.Run(() => competition?.RelevantItems.OrderByDescending(x => ratingof(x)).ToList()
+                                                ?? ItemManager.Items.OrderBy(x => x.Id).ToList());
+        }
+        double? ratingof(Item item) => competition?.RatingOf(item)?.CiLowerBound ?? null;        
+        int horizontalItems = 7;
+        foreach (Item item in _items!.Skip(_index))
+        {
+            if (++_index % 21 == 0)
+                break;
+            ItemsHolder.Add(new ThumbnailView(item, 1920 / horizontalItems - 10, $"{ratingof(item):F2}" ?? item.Id.ToString()));
+        }
+    }
 }
 
--- maui-app/Views/CompetitionSelector.xaml.cs
+++ maui-app/Views/CompetitionSelector.xaml.cs
@@ -3,12 +3,19 @@ namespace d9.ucm;
 public partial class CompetitionSelector : ContentView
 {
     public Competition? Competition { get; set; } = null;
+    public bool HasValidName => CreateButton.Text.Length > 0;
 	public CompetitionSelector()
 	{
 		InitializeComponent();
 	}
     private void CompetitionName_TextChanged(object sender, TextChangedEventArgs e)
     {
+        if (!HasValidName)
+        {
+            CreateButton.Text = "Invalid name!";
+            CreateButton.IsEnabled = false;
+        }
+        CreateButton.IsEnabled = true;
         if (File.Exists(Competition.PathFor(CompetitionName.Text)))
         {
             CreateButton.Text = "Load";
@@ -20,6 +27,11 @@ public partial class CompetitionSelector : ContentView
     }
     private async void CreateCompetition(object sender, EventArgs e)
     {
+        if(!HasValidName)
+        {
+            Competition = null;
+            return;
+        }
         Competition = await Competition.LoadOrCreateAsync(CompetitionName.Text);
         CompetitionCreated?.Invoke(sender, e);
     }
