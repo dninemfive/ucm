25119a42dbf199ce7b06dd559b01664c690fc68c
dninemfive <11084010+dninemfive@users.noreply.github.com>
2023-09-21 00:21:35 -0700

    work on loading urls

--- maui-app/Pages/MainPage.xaml.cs
+++ maui-app/Pages/MainPage.xaml.cs
@@ -1,10 +1,22 @@
-﻿namespace d9.ucm;
+﻿using System.Text.Json;
+
+namespace d9.ucm;
 
 public partial class MainPage : ContentPage
 {
 	public MainPage()
 	{
         InitializeComponent();
+		string rulePath = @"C:\Users\dninemfive\Documents\workspaces\misc\ucm\maui-app\urlrule.json.secret",
+			   bookmarksPath = @"C:\Users\dninemfive\Documents\workspaces\misc\ucm\bookmark-plugin\ucm-bookmarks-firefox\bookmarks.json";
+        UrlRule rule = JsonSerializer.Deserialize<UrlRule>(File.ReadAllText(rulePath))!;
+		List<string> bookmarks = JsonSerializer.Deserialize<List<string>>(File.ReadAllText(bookmarksPath))!;
+		foreach(string s in bookmarks)
+		{
+			if (rule.Supports(s))
+				Text.Text = rule.ApiUrl(s);
+			break;
+		}
 	}
 }
 
---
+++ maui-app/UrlRule.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace d9.ucm;
+public class UrlRule
+{
+    [JsonInclude]
+    public string MatchRegex { get; private set; }
+    [JsonInclude]
+    public string IdRegex { get; private set; }
+    [JsonInclude]
+    public string Prefix { get; private set; }
+    [JsonInclude]
+    public string Suffix { get; private set; }
+    [JsonConstructor]
+    public UrlRule(string matchRegex, string idRegex, string prefix, string suffix)
+    {
+        MatchRegex = matchRegex;
+        IdRegex = idRegex;
+        Prefix = prefix;
+        Suffix = suffix;
+    }
+    public bool Supports(string url)
+        => Regex.IsMatch(url, MatchRegex);
+    public string ApiUrl(string url)
+        => $"{Prefix}{Regex.Match(url, IdRegex)}{Suffix}";
+}
