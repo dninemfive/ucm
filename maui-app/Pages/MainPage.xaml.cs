f8977645226e7a29d974ab320c338bfe0c58d4ac
dninemfive <11084010+dninemfive@users.noreply.github.com>
2023-09-27 22:47:19 -0700

    maybe found working thing

--- maui-app/Pages/MainPage.xaml.cs
+++ maui-app/Pages/MainPage.xaml.cs
@@ -16,23 +16,26 @@ public partial class MainPage : ContentPage
 
     private async void Button_Clicked(object sender, EventArgs e)
     {
-        UrlRule exampleRule = new("https://example.com/.+", "/.+", "https://example.com", "?q=test", new List<(string, string)>() { ("User-Agent", "ucm") });
-        File.WriteAllText("C:/Users/dninemfive/Pictures/misc/ucm/exampleUrlRule.json",
-            JsonSerializer.Serialize(exampleRule, new JsonSerializerOptions() { WriteIndented = true }));
-        Text.Text = "done";
-        return;
-        foreach (Item item in ItemManager.Items)
-            await item.SaveAsync();
-        Text.Text = "done!";
-        return;
-        string rulePath = @"C:\Users\dninemfive\Documents\workspaces\misc\ucm\maui-app\urlrule.json.secret",
-               bookmarksPath = @"C:\Users\dninemfive\Documents\workspaces\misc\ucm\bookmark-plugin\ucm-bookmarks-firefox\bookmarks.json",
-               keyPath = @"C:\Users\dninemfive\Documents\workspaces\misc\ucm\maui-app\Keys\test.key";
-        UrlRule rule = JsonSerializer.Deserialize<UrlRule>(File.ReadAllText(rulePath))!;
-        List<string> bookmarks = JsonSerializer.Deserialize<List<string>>(File.ReadAllText(bookmarksPath))!;
-        string url = bookmarks.Where(rule.Supports).First();
-        // HttpClient client = new();
-        // Text.Text = await client.GetStringAsync(url);
+        UrlRule? urlRule = JsonSerializer.Deserialize<UrlRule>(
+            File.ReadAllText(@"C:\Users\dninemfive\Documents\workspaces\misc\ucm\common\urlrule\secret1.json.secret"));
+        if(urlRule is null)
+        {
+            Text.Text = "UrlRule was null";
+            return;
+        }
+        string supportedUrl = JsonSerializer.Deserialize<List<string>>
+            (File.ReadAllText(@"C:\Users\dninemfive\Documents\workspaces\misc\ucm\bookmark-plugin\ucm-bookmarks-firefox\bookmarks.json"))!
+            .Where(urlRule.Supports)
+            .First();        
+        HttpRequestMessage msg = urlRule.RequestMessageFor(supportedUrl);
+        Text.Text = msg.RequestUri!.AbsoluteUri;
+        Utils.Log(JsonSerializer.Serialize(msg, new JsonSerializerOptions() { WriteIndented = true }));
+        HttpClient client = new();
+        HttpResponseMessage response = await client.SendAsync(msg);
+        Utils.Log(JsonSerializer.Serialize(response, new JsonSerializerOptions() { WriteIndented = true }));
+        Text.Text = JsonSerializer.Serialize(response.Content, new JsonSerializerOptions() { WriteIndented = true });
+        client.DefaultRequestHeaders.Add("User-Agent", urlRule.Headers["User-Agent"]);
+        Utils.Log(JsonSerializer.Serialize(await client.GetStringAsync(supportedUrl)));
     }
 }
 
--- maui-app/UrlRule.cs
+++ maui-app/UrlRule.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Primitives;
+﻿using d9.utl;
+using Microsoft.Extensions.Primitives;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +12,8 @@ namespace d9.ucm;
 public class UrlRule
 {
     [JsonInclude]
+    public string Name { get; private set; }
+    [JsonInclude]
     public string MatchRegex { get; private set; }
     [JsonInclude]
     public string IdRegex { get; private set; }
@@ -19,12 +22,11 @@ public class UrlRule
     [JsonInclude]
     public string Suffix { get; private set; }
     [JsonInclude]
-    public List<(string, string)> Headers { get; private set; }
-    public UrlRule(string matchRegex, string idRegex, string prefix, string suffix, params (string, string)[] headers)
-        : this(matchRegex, idRegex, prefix, suffix, headers.ToList()) { }
+    public Dictionary<string, string> Headers { get; private set; }
     [JsonConstructor]
-    public UrlRule(string matchRegex, string idRegex, string prefix, string suffix, List<(string, string)> headers)
+    public UrlRule(string name, string matchRegex, string idRegex, string prefix, string suffix, Dictionary<string, string> headers)
     {
+        Name = name;
         MatchRegex = matchRegex;
         IdRegex = idRegex;
         Prefix = prefix;
@@ -35,4 +37,22 @@ public class UrlRule
         => Regex.IsMatch(url, MatchRegex);
     public string UrlFor(string url) 
         => $"{Prefix}{Regex.Match(url, IdRegex)}{Suffix}";
+    public HttpRequestMessage RequestMessageFor(string url, HttpMethod? method = null)
+    {
+        if(!Supports(url))
+        {
+            throw new ArgumentException($"UrlRule {Name} does not match url {url}!", nameof(url));
+        }
+        Utils.Log($"`{Name}`.RequestMessageFor({url}, {method.PrintNull()})");
+        HttpRequestMessage result = new(method ?? HttpMethod.Get, UrlFor(url));
+        foreach((string key, string value) in Headers.OrderBy(x => x.Key))
+        {
+            result.Headers.Add(key, value);
+        }
+        return result;
+    }
+    public PendingItem PendingItemFor(string url)
+    {
+        throw new NotImplementedException();
+    }
 }
