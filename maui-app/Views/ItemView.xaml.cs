17f3b2e7bffee7bce66115bb98ae049e0c6830f1
dninemfive <11084010+dninemfive@users.noreply.github.com>
2023-10-06 01:11:53 -0700

    initial Itemview

---
+++ maui-app/Views/ItemView.xaml.cs
@@ -0,0 +1,34 @@
+namespace d9.ucm;
+
+public partial class ItemView : ContentView
+{
+	private IItemViewable? _item = null;
+	public IItemViewable? Item
+	{
+		get => _item;
+		set
+		{
+			_item = value;
+			if (value is null)
+			{
+                ContentHolder.Content = null;
+				SourceList.Children.Clear();
+				return;
+            }
+			ContentHolder.Content = _item!.View;
+			foreach(string labelText in _item.ItemSources.Select(x => x.LabelText))
+			{
+				SourceList.Add(new Label()
+				{
+					Text = labelText,
+					BackgroundColor = Colors.LightGrey,
+					TextColor = Colors.Black
+				});
+			}
+		}
+	}
+	public ItemView()
+	{
+		InitializeComponent();
+	}
+}
