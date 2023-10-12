3772d3c71db1b04ff962d991b27b14b3aebfe291
dninemfive <11084010+dninemfive@users.noreply.github.com>
2023-10-12 15:54:59 -0700

    AAAAAAA

--- maui-app/Views/NavigationView.xaml.cs
+++ maui-app/Views/NavigationView.xaml.cs
@@ -9,6 +9,7 @@ public partial class NavigationView : ContentView
         {
             ResultPage = resultPage;
         }
+        public override string ToString() => $"{ResultPage}";
 #pragma warning disable IDE0001 // "name can be simplified": intentionally using full name to clarify
         public static implicit operator NavigationView.EventArgs(int z) => new(z);
         public static implicit operator int(NavigationView.EventArgs nvea) => nvea.ResultPage;
@@ -65,6 +66,7 @@ public partial class NavigationView : ContentView
     private void LastItemButton_Clicked(object sender, System.EventArgs e) => CurrentIndex = MaxPage;
     public void UpdateButtonActivation()
     {
+        Utils.Log($"UpdateButtonActivation()");
         FirstItemButton.IsEnabled = CurrentIndex > 0;
         PreviousItemButton.IsEnabled = CurrentIndex > 0;
         NextItemButton.IsEnabled = CurrentIndex < MaxPage;
@@ -72,11 +74,12 @@ public partial class NavigationView : ContentView
         foreach(IView iv in NumericalButtonHolder.Children)
         {
             if (iv is Button b)
-                b.IsEnabled = IndexOf(b) == CurrentIndex; 
+                b.IsEnabled = IndexOf(b) != CurrentIndex; 
         }
     }
     public void AddButton(int index)
     {
+        Utils.Log($"AddButton({index})");
         Button button = new()
         {
             Text = $"{index + 1}",
@@ -84,12 +87,15 @@ public partial class NavigationView : ContentView
             WidthRequest = 40
         };
         button.Clicked += (sender, e) => CurrentIndex = IndexOf((sender as Button)!);
+        NumericalButtonHolder.Add(button);
     }
     private static int IndexOf(Button b) => int.Parse(b.Text) - 1;
     public void CorrectButtonCount()
     {
+        
         int initialCt = NumericalButtonHolder.Children.Count, target = Math.Min(MaxPage, MaxNumericalButtons);
-        if(initialCt < target)
+        Utils.Log($"CorrectButtonCount({initialCt} -> {target})");
+        if (initialCt < target)
         {
             for (int i = NumericalButtonHolder.Children.Count; i < Math.Min(MaxPage, MaxNumericalButtons); i++)
                 AddButton(i);
@@ -103,9 +109,9 @@ public partial class NavigationView : ContentView
     }
     private void UpdateButtonIndices()
     {
+        Utils.Log($"UpdateButtonIndices()");
         int min = _currentIndex - MaxNumericalButtons / 2,
-            buttonCt = NumericalButtonHolder.Children.Count;
-        
+            buttonCt = NumericalButtonHolder.Children.Count;        
         if(min + buttonCt > MaxPage)
         {
             min = MaxPage - buttonCt;
