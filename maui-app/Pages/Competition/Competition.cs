497cb18dd270cae70d442b8f112f7b7dab77edc5
dninemfive <11084010+dninemfive@users.noreply.github.com>
2023-11-09 00:59:22 -0800

    start competition refactor

--- maui-app/Pages/Competition/Competition.cs
+++ maui-app/Pages/Competition/Competition.cs
@@ -10,64 +10,7 @@ using System.Threading.Tasks;
 namespace d9.ucm;
 public class Competition
 {
-    [JsonInclude]
-    public string Name { get; private set; }
-    [JsonInclude]
-    public HashSet<ItemId> IrrelevantItems { get; private set; } = new();
-    public class Rating
-    {
-        [JsonInclude]
-        public int TimesSelected { get; set; } = 0;
-        [JsonInclude]
-        public int TotalRatings { get; set; } = 0;
-        [JsonIgnore]
-        public double Ratio => TimesSelected / (double)TotalRatings;
-        [JsonIgnore]
-        private const double _z = 1.644853627; // result of `=NORM.S.INV(0.95)` in Excel, equivalent to Statistics2.pnormaldist(0.95)
-        [JsonIgnore]
-        public double CiLowerBound
-            => TotalRatings == 0 ? 0 : (Ratio + _z * _z / (2 * TotalRatings) - _z * Math.Sqrt((Ratio * (1 - Ratio) + _z * _z / (4 * TotalRatings)) / TotalRatings)) / (1 + _z * _z / TotalRatings);
-        [JsonIgnore]
-        public double CiUpperBound
-            => TotalRatings == 0 ? 0 : (Ratio + _z * _z / (2 * TotalRatings) + _z * Math.Sqrt((Ratio * (1 - Ratio) + _z * _z / (4 * TotalRatings)) / TotalRatings)) / (1 + _z * _z / TotalRatings);
-        [JsonIgnore]
-        public double CiCenter
-            => (CiLowerBound + CiUpperBound) / 2;
-        [JsonIgnore]
-        public double MarginOfError
-            => (CiUpperBound - CiLowerBound) / 2;
-        [JsonIgnore]
-        public double Weight => WeightFunction(TotalRatings);
-        public static double WeightFunction(int i) => i > 0 ? 1 / (double)(i * i) : 10.0;
-        public bool ShouldShow(double percentile) => CiLowerBound > percentile; // TotalRatings < 7 || CiUpperBound >= 0.42;
-        [JsonConstructor]
-        public Rating(int timesSelected, int totalRatings)
-        {
-            TimesSelected = timesSelected;
-            TotalRatings = totalRatings;
-        }
-        public override string ToString() => $"{TimesSelected}/{TotalRatings} ({CiCenter:F2}±{MarginOfError:F2})";
-        public void Increment(bool selected)
-        {
-            if (selected)
-                TimesSelected++;
-            TotalRatings++;
-        }
-    }
-    [JsonInclude]
-    public Dictionary<ItemId, Rating> Ratings { get; set; } = new();
-    [JsonIgnore]
-    public IEnumerable<Rating> RelevantRatings => RelevantItems.Select(RatingOf).Where(x => x is not null)!;
-    public double ThresholdPercentile { get; set; } = 0;
-    [JsonIgnore]
-    public IEnumerable<Rating> ShownRatings
-    {
-        get
-        {
-            double threshold = RelevantRatings.Select(x => x.CiLowerBound).Percentile(ThresholdPercentile);
-            return RelevantRatings.Where(x => x.CiLowerBound > threshold); //.Concat(RelevantUnratedItems.Select(RatingOf))!;
-        }
-    }
+    #region constructors
     public Competition(string name)
     {
         Name = name;
@@ -79,6 +22,70 @@ public class Competition
         IrrelevantItems = irrelevantItems;
         Ratings = ratings;
     }
+    #endregion constructors
+    #region serialized properties
+    [JsonInclude]
+    public HashSet<ItemId> IrrelevantItems { get; private set; } = new();
+    [JsonInclude]
+    public string Name { get; private set; }
+    [JsonInclude]
+    public Dictionary<ItemId, Rating> Ratings { get; set; } = new();
+    #endregion serialized properties
+    #region nonserialized properties
+    [JsonIgnore]
+    public string FilePath => PathFor(Name);
+    [JsonIgnore]
+    public Item NextItem
+    {
+        get
+        {
+            double threshold = RelevantRatings.Select(x => x.CiLowerBound).Percentile(ThresholdPercentile);
+            Utils.Log($"Threshold: {threshold:F2}");
+            Item result = RelevantItems.Where(x => x.Id != _previousItem?.Id && (RatingOf(x)?.ShouldShow(threshold) ?? true))
+                                       .WeightedRandomElement(x => RatingOf(x)?.Weight ?? Rating.WeightFunction(0));
+            Utils.Log($"NextItem -> {result} {(ulong)result.Id}");
+            _previousItem = result;
+            return result;
+        }
+    }
+    [JsonIgnore]
+    public IEnumerable<Item> RelevantItems => ItemManager.Items.Where(x => !IsIrrelevant(x.Id));
+    [JsonIgnore]
+    public IEnumerable<Rating> RelevantRatings => RelevantItems.Select(RatingOf).Where(x => x is not null)!;
+    [JsonIgnore]
+    public IEnumerable<Rating> ShownRatings
+    {
+        get
+        {
+            double threshold = RelevantRatings.Select(x => x.CiLowerBound).Percentile(ThresholdPercentile);
+            return RelevantRatings.Where(x => x.CiLowerBound > threshold); //.Concat(RelevantUnratedItems.Select(RatingOf))!;
+        }
+    }
+    [JsonIgnore]
+    public double ThresholdPercentile { get; set; } = 0;
+    #endregion nonserialized properties
+    #region static properties/methods
+    public static async Task<Competition?> LoadOrCreateAsync(string? name)
+    {
+        if (name is null or "")
+            return null;
+        string path = PathFor(name);
+        if (File.Exists(path))
+        {
+            return await Task.Run(() => JsonSerializer.Deserialize<Competition>(File.ReadAllText(path))!);
+        }
+        else
+        {
+            Competition result = new(name);
+            await result.SaveAsync();
+            return result;
+        }
+    }
+    public static Competition? Named(string? name)
+        => name is null ? null : CompetitionManager.CompetitionsByName.TryGetValue(name, out Competition? competition) ? competition : null;
+    public static IEnumerable<string> Names => CompetitionManager.Names;
+    public static string PathFor(string name) => Path.Join(MauiProgram.TEMP_COMP_LOCATION, $"{name}.json");
+    #endregion static properties/methods
     public bool IsIrrelevant(ItemId id) => IrrelevantItems.Contains(id) || !ItemManager.ItemsById.TryGetValue(id, out Item? item) || item.Hidden;
     public Rating? RatingOf(ItemId id)
         => RatingOf(ItemManager.TryGetItemById(id));
@@ -101,9 +108,6 @@ public class Competition
         }
         return new(timesSelected, totalRatings);
     }
-    public static Competition? Named(string? name) 
-        => name is null ? null : CompetitionManager.CompetitionsByName.TryGetValue(name, out Competition? competition) ? competition : null;
-    public static IEnumerable<string> Names => CompetitionManager.Names;
     [JsonIgnore]
     public Item? Left, Right;
     public Item this[Side side] {
@@ -159,29 +163,9 @@ public class Competition
             throw new ArgumentException($"Neither the left ({this[Side.Left].Id}) or right ({this[Side.Right].Id}) ids match {id.PrintNull()}!");
         }
     }
-    [JsonIgnore]
-    public IEnumerable<Item> RelevantItems => ItemManager.Items.Where(x => !IsIrrelevant(x.Id));
     private Item? _previousItem = null;
-    [JsonIgnore]
-    public Item NextItem
-    {
-        get
-        {
-            double threshold = RelevantRatings.Select(x => x.CiLowerBound).Percentile(ThresholdPercentile);
-            Utils.Log($"Threshold: {threshold:F2}");
-            Item result = RelevantItems.Where(x => x.Id != _previousItem?.Id && (RatingOf(x)?.ShouldShow(threshold) ?? true))
-                                       .WeightedRandomElement(x => RatingOf(x)?.Weight ?? Rating.WeightFunction(0));
-            Utils.Log($"NextItem -> {result} {(ulong)result.Id}");
-            _previousItem = result;
-            return result;
-        }
-    }
     public void NextItems()
         => (Left, Right) = (NextItem, NextItem);
-    public void MarkIrrelevant(Side side)
-    {
-        _ = IrrelevantItems.Add(this[side].Id);
-    }
     public void SetIrrelevant(ItemId? id, bool value)
     {
         if (id is null)
@@ -197,31 +181,52 @@ public class Competition
     }
     public void ToggleIrrelevant(ItemId id)
         => SetIrrelevant(id, !IsIrrelevant(id));
-    public static async Task<Competition?> LoadOrCreateAsync(string? name)
-    {
-        if (name is null or "")
-            return null;
-        string path = PathFor(name);
-        if (File.Exists(path))
-        {
-            return await Task.Run(() => JsonSerializer.Deserialize<Competition>(File.ReadAllText(path))!);
-        }
-        else
-        {
-            Competition result = new(name);
-            await result.SaveAsync();
-            return result;
-        }
-    }
-    public static string PathFor(string name) => Path.Join(MauiProgram.TEMP_COMP_LOCATION, $"{name}.json");
-    [JsonIgnore]
-    public string FilePath => PathFor(Name);
     public async Task SaveAsync()
     {
-        await File.WriteAllTextAsync(FilePath, JsonSerializer.Serialize(this, new JsonSerializerOptions() { WriteIndented = true}));
+        await File.WriteAllTextAsync(FilePath, JsonSerializer.Serialize(this, new JsonSerializerOptions() { WriteIndented = true }));
     }
     public Rating? RatingOf(Side side) 
         => RatingOf(this[side].Id);
+    public class Rating
+    {
+        [JsonInclude]
+        public int TimesSelected { get; set; } = 0;
+        [JsonInclude]
+        public int TotalRatings { get; set; } = 0;
+        [JsonIgnore]
+        public double Ratio => TimesSelected / (double)TotalRatings;
+        [JsonIgnore]
+        private const double _z = 1.644853627; // result of `=NORM.S.INV(0.95)` in Excel, equivalent to Statistics2.pnormaldist(0.95)
+        [JsonIgnore]
+        public double CiLowerBound
+            => TotalRatings == 0 ? 0 : (Ratio + _z * _z / (2 * TotalRatings) - _z * Math.Sqrt((Ratio * (1 - Ratio) + _z * _z / (4 * TotalRatings)) / TotalRatings)) / (1 + _z * _z / TotalRatings);
+        [JsonIgnore]
+        public double CiUpperBound
+            => TotalRatings == 0 ? 0 : (Ratio + _z * _z / (2 * TotalRatings) + _z * Math.Sqrt((Ratio * (1 - Ratio) + _z * _z / (4 * TotalRatings)) / TotalRatings)) / (1 + _z * _z / TotalRatings);
+        [JsonIgnore]
+        public double CiCenter
+            => (CiLowerBound + CiUpperBound) / 2;
+        [JsonIgnore]
+        public double MarginOfError
+            => (CiUpperBound - CiLowerBound) / 2;
+        [JsonIgnore]
+        public double Weight => WeightFunction(TotalRatings);
+        public static double WeightFunction(int i) => i > 0 ? 1 / (double)(i * i) : 10.0;
+        public bool ShouldShow(double percentile) => CiLowerBound > percentile; // TotalRatings < 7 || CiUpperBound >= 0.42;
+        [JsonConstructor]
+        public Rating(int timesSelected, int totalRatings)
+        {
+            TimesSelected = timesSelected;
+            TotalRatings = totalRatings;
+        }
+        public override string ToString() => $"{TimesSelected}/{TotalRatings} ({CiCenter:F2}±{MarginOfError:F2})";
+        public void Increment(bool selected)
+        {
+            if (selected)
+                TimesSelected++;
+            TotalRatings++;
+        }
+    }
 }
 public enum Side { Left, Right, None }
 public static class SideExtensions
